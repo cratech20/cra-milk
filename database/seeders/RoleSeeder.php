a8ff20527e4717a1330f1935b2468686df74849c
vipblogger <t3924@ya.ru>
2021-03-15 13:18:43 +0300

    Initial commit

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        
+    }
+}
