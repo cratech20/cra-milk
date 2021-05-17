b38f50e36213061ae7dfffccfb7a00bef13af44a
vipblogger <t3924@ya.ru>
2021-05-17 23:20:05 +0300

    fix

--- resources/views/cabinet/admin.blade.php
+++ resources/views/cabinet/admin.blade.php
@@ -9,9 +9,9 @@
 
                     <div class="card-body">
                         <a href="{{ route('reports.liters') }}">Отчёт PowerBI литры</a><br>
-                        <a href="{{ route('reports.impulse') }}">Отчёт PowerBI импульсы</a>
+                        <a href="{{ route('reports.impulse') }}">Отчёт PowerBI импульсы</a><br>
                         <a href="https://functions.yandexcloud.net/d4eks53mf45uqn38bvn1">Последние сообщения с
-                            устройств</a>
+                            устройств</a><br>
                     </div>
                 </div>
             </div>
