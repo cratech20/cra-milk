656a58a9e8556d67c4b5c09d67003171a63882ab
Ruslan00 <yamaletdinov.rusl@yandex.ru>
2021-10-05 14:37:20 +0500

    Отчеты. Новый отчет

--- app/Services/Reports/Generators/LitersByHourPeriodsGenerator.php
+++ app/Services/Reports/Generators/LitersByHourPeriodsGenerator.php
@@ -6,6 +6,7 @@ namespace App\Services\Reports\Generators;
 use App\Models\User;
 use App\Services\Reports\DataFillers\LitersByHourPeriodsDataFiller;
 use App\Services\Reports\DataFillers\LitersByHourPeriodsDataFiller2;
+use App\Services\Reports\DataFillers\LitersByHourPeriodsDataFiller3;
 use App\Services\Reports\LitersByHourPeriodsReport;
 use App\Services\Reports\LitersByHourReport;
 use App\Services\Reports\LitersPeriodsReport;
@@ -18,7 +19,7 @@ class LitersByHourPeriodsGenerator
             $dataFiller = new LitersByHourPeriodsDataFiller($datePeriod, $hourPeriods, $user, $isAdmin, new LitersByHourPeriodsReport());
             $report = $dataFiller->process();
         } elseif ($report == 'hour') {
-            $dataFiller = new LitersByHourPeriodsDataFiller($datePeriod, $hourPeriods, $user, $isAdmin, new LitersByHourReport());
+            $dataFiller = new LitersByHourPeriodsDataFiller3($datePeriod, $hourPeriods, $user, $isAdmin, new LitersByHourReport());
             $report = $dataFiller->process();
         } else {
             $dataFiller = new LitersByHourPeriodsDataFiller2($datePeriod, $hourPeriods, $user, $isAdmin, new LitersPeriodsReport());
