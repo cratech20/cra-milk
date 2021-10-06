51c5a8844efc6618c35aeca706300d8e12bd18d1
Ruslan00 <yamaletdinov.rusl@yandex.ru>
2021-10-06 10:05:38 +0500

    Внутренний номер коровы

--- app/Services/Reports/LitersByHourReport.php
+++ app/Services/Reports/LitersByHourReport.php
@@ -171,7 +171,7 @@ class LitersByHourReport
             if ($cow) {
                 $cowInternalId = $cow['internal_code'];
             } else {
-                $cowInternalId = '123';
+                $cowInternalId = '123456';
             }
             $group = $cows[$cowId]->group->calculated_name ?? 'Неизвестно';
             $body[$cowId] = [$deviceName, $cowName, $group, $cowNum];
